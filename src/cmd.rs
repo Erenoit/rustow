4c453b672a7b9313c096f17f650496201f8ea791
Eren Önen <erenoit@protonmail.com>
2023-08-18 00:47:19 +0300

    feat: use clap for command line arguments

---
+++ src/cmd.rs
@@ -0,0 +1,100 @@
+use std::path::PathBuf;
+
+use clap::{arg, command, ArgAction, Parser, ValueHint};
+
+#[derive(Parser)]
+#[command(author, version)]
+pub struct Args {
+    /// The directory containing the packages to be stowed.
+    #[arg(
+        short = 'd',
+        long,
+        value_name = "DIR",
+        value_hint = ValueHint::DirPath,
+        default_value = ".",
+    )]
+    pub stow_dir: PathBuf,
+
+    /// The directory in which the packages will be stowed.
+    #[arg(
+        short = 't',
+        long,
+        value_name = "DIR",
+        value_hint = ValueHint::DirPath,
+        default_value = "..",
+    )]
+    pub target_dir: PathBuf,
+
+    /// Enable verbose output.
+    #[arg(short, long, default_value = "false")]
+    pub verbose: bool,
+
+    /// Enable simulation mode.
+    #[arg(short, long, default_value = "false")]
+    pub simulate: bool,
+
+    /// Disable the special keywords feature.
+    #[arg(long, default_value = "false")]
+    pub no_special_keywords: bool,
+
+    /// Disable the security checks.
+    #[arg(
+        long,
+        default_value = "false",
+        default_value_if("no-special-keywords", "true", "true")
+    )]
+    pub no_security_check: bool,
+
+    /// Stow the package.
+    /// Creates symlinks of files in the package to target directory
+    #[arg(
+        short = 'S',
+        long,
+        value_name = "PACKAGE",
+        action = ArgAction::Append,
+        value_hint = ValueHint::FilePath,
+        required_unless_present_any = ["unstow", "restow", "adopt"],
+        next_line_help = true,
+    )]
+    pub stow: Vec<PathBuf>,
+
+    /// Unstow the package.
+    /// Removes existing symlinks of files in the package in target directory
+    #[arg(
+        short = 'D',
+        long,
+        value_name = "PACKAGE",
+        action = ArgAction::Append,
+        value_hint = ValueHint::FilePath,
+        required_unless_present_any = ["stow", "restow", "adopt"],
+        next_line_help = true,
+    )]
+    pub unstow: Vec<PathBuf>,
+
+    /// Restow the package.
+    /// Same as unstowing and stowing a package
+    #[arg(
+        short = 'R',
+        long,
+        value_name = "PACKAGE",
+        action = ArgAction::Append,
+        value_hint = ValueHint::FilePath,
+        required_unless_present_any = ["stow", "unstow", "adopt"],
+        next_line_help = true,
+    )]
+    pub restow: Vec<PathBuf>,
+
+    /// Adopt the package.
+    /// Imports existing files in target directory to stow package. USE WITH
+    /// CAUTION!
+    #[arg(
+        short = 'A',
+        long,
+        value_name = "PACKAGE",
+        action = ArgAction::Append,
+        value_hint = ValueHint::FilePath,
+        required_unless_present_any = ["stow", "unstow", "restow"],
+        next_line_help = true,
+    )]
+    pub adopt: Vec<PathBuf>,
+}
